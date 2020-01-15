08e2e7a3e1b1f7fab6ba4a554f89542de1118b8e
Sotirios Nikoloutsopoulos <sotirisnik@gmail.com>
2020-01-15 11:51:12 +0200

    updates loris's menu interface

---
+++ sql/update_interface.sql
@@ -0,0 +1,8 @@
+--update user interface
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='2';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='4';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='5';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='30';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='31';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='32';
+UPDATE LORIS.LorisMenu SET Visible='false' WHERE ID='34';
