3bfc3d69bd93a27aa1e6458a740e8abd45bdd699
iosif <iosifspart@yahoo.gr>
2020-03-11 14:30:51 +0200

    fixed export batch issue, the produce_export.sh didnt create the batch folder and its files in nifti_out folders

--- loris/htdocs/produce_exports.sh
+++ loris/htdocs/produce_exports.sh
@@ -1,2 +1,2 @@
-/data/loris/bin/mri/python_virtualenvs/loris-mri-python/bin/python /data/loris/python/produce_qc_csv.py
-
+cd /data/loris/python
+/data/loris/bin/mri/python_virtualenvs/loris-mri-python/bin/python produce_qc_csv.py
